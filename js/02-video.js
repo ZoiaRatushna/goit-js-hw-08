b25b3979ac6b0650debf40e7be2252c1eba2e07d
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-10-16 23:40:22 +0300

    hw_8_task2_update

--- js/02-video.js
+++ js/02-video.js
@@ -1,94 +1,38 @@
 
 import Player from '@vimeo/player';
+const _ = require('lodash.throttle');
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
-//плеєр on
-player.on('play', function() {
-    console.log('played the video!');
-});
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
+myStorage = window.localStorage;
+let needTime = localStorage.getItem('videoplayer-current-time');
 
-player.on('play', onPlay);
+if (needTime) {
+    player.setCurrentTime(needTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
 
+            default:
+                // some other error occurred
+                break;
+        }
+    });  
+}
 
-// Вимкнути on-плеєр   Видалити обробник події для вказаної події. 
-//Буде видалено всіх слухачів цієї події, якщо зворотний виклик не передано, 
-//або лише цей конкретний зворотний виклик, якщо він переданий
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
+const setData = _(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data.seconds);
+}, 1000);
 
-player.on('play', onPlay);
-
-// If later on you decide that you don’t need to listen for play anymore.
-player.off('play', onPlay);
-
-// Alternatively, `off` can be called with just the event name to remove all
-// listeners.
-player.off('play');
-
-
-//Завантажте нове відео в це вставлення. Обіцянка буде вирішена, якщо відео буде
-//успішно завантажено, або воно буде відхилено, якщо його не вдалося завантажити.
-
-player.loadVideo(76979871).then(function(id) {
-    // the video successfully loaded
-}).catch(function(error) {
-    switch (error.name) {
-        case 'TypeError':
-            // the id was not a number
-            break;
-
-        case 'PasswordError':
-            // the video is password-protected and the viewer needs to enter the
-            // password first
-            break;
-
-        case 'PrivacyError':
-            // the video is password-protected or private
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
+player.on('timeupdate', function(data) {
+    // data is an object containing properties specific to that event 
+    setData(data);
 });
 
-//Запускати функцію, коли iframe програвача ініціалізовано.
-//Вам не потрібно чекати готовності до запуску, щоб почати додавати прослуховувачі
-//подій або викликати інші методи.
-
-player.ready().then(function() {
-    // the player is ready
-});
-
-//Увімкніть текстову доріжку з указаною мовою та, за бажанням, із зазначеним типом (підписи або субтитри).
-//Якщо встановити через API, мова доріжки не змінить збережені налаштування користувача.
-
-player.enableTextTrack('en').then(function(track) {
-    // track.language = the iso code for the language
-    // track.kind = 'captions' or 'subtitles'
-    // track.label = the human-readable label
-}).catch(function(error) {
-    switch (error.name) {
-        case 'InvalidTrackLanguageError':
-            // no track was available with the specified language
-            break;
-
-        case 'InvalidTrackError':
-            // no track was available with the specified language and kind
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
 
 
